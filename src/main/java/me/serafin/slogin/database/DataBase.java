bc34f9bb0c77efa536430d58bb5fa1099ea7c17e
_Serafin <54081699+Serafinowy@users.noreply.github.com>
2021-07-13 00:42:57 +0200

    Javadocs in DataBase

--- src/main/java/me/serafin/slogin/database/DataBase.java
+++ src/main/java/me/serafin/slogin/database/DataBase.java
@@ -5,12 +5,37 @@ import java.sql.SQLException;
 
 public interface DataBase {
 
+    /**
+     * Opens database connection
+     *
+     * @throws SQLException if connection can not be opened
+     */
     void openConnection() throws SQLException;
 
+    /**
+     * Closes database connection
+     *
+     * @throws SQLException if connection can not be closed
+     */
     void closeConnection() throws SQLException;
 
+    /**
+     * Send SQL update prepared statement. Symbol '?' is replaced by parameter
+     *
+     * @param command sql command string
+     * @param params  parameters table
+     * @throws SQLException if statement can not be send
+     */
     void update(String command, String... params) throws SQLException;
 
+    /**
+     * Send SQL query prepared statement. Symbol '?' is replaced by parameter
+     *
+     * @param command sql command string
+     * @param params  parameters table
+     * @return result in ResultSet object
+     * @throws SQLException if statement can not be send
+     */
     ResultSet query(String command, String... params) throws SQLException;
 
 }
